a192bbc2dadfa89a17f70b06ff6a10667dd91cc9
madhanmanilalap <35652995+madhanmanilalap@users.noreply.github.com>
2018-03-03 15:14:25 +0530

    Create R3e.c

---
+++ R3e.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[10];
+int i,b=0;
+clrscr();
+printf("Enter the string:");
+gets(a);
+for(i=0;a[i]!='\0';i++)
+{
+b++;
+}
+for(i=1;i<=b;i++)
+{
+if(i%2!=0)
+{
+printf("%c",a[i-1]);
+}}
+printf("\t");
+for(i=0;i<=b;i++)
+{
+if(i%2!=0)
+{
+printf("%c",a[i]);    
+}
+}
+getch();
+}
